78e219ba72800a20880e2ffdc497265171e0db56
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-10-20 10:41:43 -0300

    Add Routes

--- lib/app/routes/router.dart
+++ lib/app/routes/router.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mygoals/app/routes/routes.dart';
+import 'package:mygoals/app/ui/pages/home_page.dart';
+import 'package:mygoals/app/ui/pages/login_page.dart';
 
 class CustomRouter {
-  static Map<String, WidgetBuilder> routes = {};
+  static Map<String, WidgetBuilder> routes = {
+    Routes.login: (_) => const LoginPage(),
+    Routes.home: (_) => const HomePage(),
+  };
 
   static Route<dynamic> onGenerateUnknowedRoute(RouteSettings settings) {
     return MaterialPageRoute(builder: (context) {
--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,3 +1,4 @@
 class Routes {
   static const login = 'login';
+  static const home = 'home';
 }
