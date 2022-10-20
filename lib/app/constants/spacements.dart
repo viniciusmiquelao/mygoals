510862a69edab54048b4c8c0116c58029bac7f44
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-10-19 21:20:34 -0300

    Crate Spacements constant

---
+++ lib/app/constants/spacements.dart
@@ -0,0 +1,21 @@
+class Spacements {
+  static const double _SPACE_UNIT = 8;
+
+  /// 4
+  static const double XXS = _SPACE_UNIT / 2;
+
+  /// 8
+  static const double XS = _SPACE_UNIT;
+
+  /// 16
+  static const double S = _SPACE_UNIT * 2;
+
+  /// 24
+  static const double M = _SPACE_UNIT * 3;
+
+  /// 32
+  static const double L = _SPACE_UNIT * 4;
+
+  /// 40
+  static const double XL = _SPACE_UNIT * 5;
+}
